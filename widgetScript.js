670710995a1c58754cac1163ee4f53ae24d2ff9f
Ghvst Code <impoissants@gmail.com>
2024-05-30 12:33:35 +0100

    Create widgetScript.js

---
+++ widgetScript.js
@@ -0,0 +1,8 @@
+<script>
+    (function (w, d, s, o, f, js, fjs) {
+        w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
+        js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+        js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+    }(window, document, 'script', '_hw', 'https://cdn.jsdelivr.net/gh/Ghvstcode/wid@main/widget.js'));
+    _hw('init');
+</script>
